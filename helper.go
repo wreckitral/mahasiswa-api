5c096d4dce286d9f976386b633a4fa603040f279
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-24 21:27:05 +0700

    feat: add makeHttpHandlerFunc
    this function is created to handle error from the http HandlerFunc from
    go

--- helper.go
+++ helper.go
@@ -5,9 +5,23 @@ import (
 	"net/http"
 )
 
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type apiError struct {
+    Error string `json:"error"`
+}
+
 func writeJSON(res http.ResponseWriter, status int, body any) error {
     res.Header().Add("Content-Type", "application/json")
     res.WriteHeader(status)
 
     return json.NewEncoder(res).Encode(body)
+}
+
+func makeHttpHandlerFunc(f apiFunc) http.HandlerFunc {
+    return func(res http.ResponseWriter, req *http.Request) {
+        if err := f(res, req); err != nil {
+            writeJSON(res, http.StatusInternalServerError, apiError{err.Error()})
+        }
+    } 
 }
