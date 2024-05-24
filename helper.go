c3fff62e3ea61cf3e963b0dfec0ced06d5a5704d
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-24 21:23:01 +0700

    feat: add writeJSON function
    writeJSON is the encoder for the http response

--- helper.go
+++ helper.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(res http.ResponseWriter, status int, body any) error {
+    res.Header().Add("Content-Type", "application/json")
+    res.WriteHeader(status)
+
+    return json.NewEncoder(res).Encode(body)
+}
