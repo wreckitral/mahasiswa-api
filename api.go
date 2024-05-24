9aaa025b91db69ced010e669c5aa91736e1259c1
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-24 21:29:22 +0700

    feat(api): create APIServer struct and NewAPIServer func

--- api.go
+++ api.go
@@ -1,1 +1,15 @@
 package main
+
+import "net/http"
+
+
+type APIServer struct {
+    listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+    return &APIServer{
+        listenAddr: listenAddr,
+    }
+}
+
